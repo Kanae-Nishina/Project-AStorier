c6b2175e054cfc4976203720e330e68f7d809944
topi0247 <karubona.ra247@gmail.com>
2024-07-12 22:24:39 +0900

    プロフィールのモデルSpec

--- back/spec/models/profile_spec.rb
+++ back/spec/models/profile_spec.rb
@@ -3,18 +3,27 @@ require 'rails_helper'
 RSpec.describe Profile, type: :model do
   describe 'textのバリデーション' do
     it 'textが空欄で登録できるか' do
-      
+      profile = build(:profile, text: '')
+      profile.valid?
+      expect(profile.errors.full_messages).to be_empty
     end
 
     it 'textが登録できるか' do
+      profile = build(:profile, text: 'test')
+      profile.valid?
+      expect(profile.errors.full_messages).to be_empty
     end
 
     it 'textが1000文字で登録できるか' do
-
+      profile = build(:profile, text: 'a' * 1000)
+      profile.valid?
+      expect(profile.errors.full_messages).to be_empty
     end
 
     it 'textが1001文字で登録できないか' do
-
+      profile = build(:profile, text: 'a' * 1001)
+      profile.valid?
+      expect(profile.errors.full_messages).to include('Text プロフィールは1000文字以内で入力してください')
     end
   end
 end
