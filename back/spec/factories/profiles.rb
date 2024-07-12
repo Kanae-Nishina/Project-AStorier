f3530e76060686498e2822ee56f4ed2e8e09a098
topi0247 <karubona.ra247@gmail.com>
2024-07-12 22:08:43 +0900

    textのテスト内容洗い出し

--- back/spec/factories/profiles.rb
+++ back/spec/factories/profiles.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :profile do
-    
+    association :user
+    text { "profile text" }
   end
 end
--- back/spec/models/profile_spec.rb
+++ back/spec/models/profile_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  
+  describe 'textのバリデーション' do
+    it 'textが空欄で登録できるか' do
+      
+    end
+
+    it 'textが登録できるか' do
+    end
+
+    it 'textが1000文字で登録できるか' do
+
+    end
+
+    it 'textが1001文字で登録できないか' do
+
+    end
+  end
 end
