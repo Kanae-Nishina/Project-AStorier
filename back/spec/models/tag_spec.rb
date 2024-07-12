ff054c9ad906769731b21738ff0ca0418da6c163
topi0247 <karubona.ra247@gmail.com>
2024-07-12 22:17:13 +0900

    タグのRSpecでFactoryBotを使うよう修正

--- back/spec/models/tag_spec.rb
+++ back/spec/models/tag_spec.rb
@@ -2,23 +2,23 @@ require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
   describe 'nameのバリデーション' do
+    it 'nameが登録できるか' do
+      tag = build(:tag, name: 'test')
+      tag.valid?
+      expect(tag.errors.full_messages).to be_empty
+    end
+
     it 'nameが空欄で登録できないか' do
-      tag = Tag.create(name: '')
+      tag = build(:tag, name: '')
       tag.valid?
       expect(tag.errors.full_messages).to include('Name タグ名を入力してください')
     end
 
     it 'nameが重複して登録できないか' do
-      tag1 = Tag.create(name: 'test')
-      tag2 = Tag.create(name: 'test')
-      tag2.valid?
-      expect(tag2.errors.full_messages).to include('Name タグ名が既に使用されています')
-    end
-
-    it 'nameが登録できるか' do
-      tag = Tag.create(name: 'test')
-      expect(tag).to be_valid
-      expect(tag.errors.full_messages).to be_empty
+      create(:tag, name: 'test')
+      tag = build(:tag, name: 'test')
+      tag.valid?
+      expect(tag.errors.full_messages).to include('Name タグ名が既に使用されています')
     end
   end
 end
