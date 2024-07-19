c7f35b89bdec29ecb73c23145609bfcf9db911fa
topi0247 <karubona.ra247@gmail.com>
2024-07-19 23:32:48 +0900

    feat:header_imageのテスト追加

--- back/spec/models/profile_spec.rb
+++ back/spec/models/profile_spec.rb
@@ -76,4 +76,54 @@ RSpec.describe Profile, type: :model do
       expect(profile.errors.full_messages).to include('Avatar 画像形式はwebpのみ対応しています')
     end
   end
+
+  describe 'header_imageのバリデーション' do
+    it 'header_imageが登録できるか' do
+      profile = create(:profile, :with_header_image)
+      profile.valid?
+      expect(profile.errors.full_messages).to be_empty
+    end
+
+    it 'header_imageが空欄で登録できるか' do
+      profile = create(:profile)
+      profile.valid?
+      expect(profile.errors.full_messages).to be_empty
+    end
+
+    it 'header_imageの容量が10MB以下で登録できるか' do
+      profile = create(:profile)
+      image_blob = FactoryBotHelpers.dummy_image('dummy.png')
+      profile.header_image.attach(io: StringIO.new(image_blob),
+                                  filename: SecureRandom.uuid,
+                                  content_type: 'image/webp')
+      expect(profile.errors.full_messages).to be_empty
+    end
+
+    it 'header_imageの容量が10MB以上で登録できないか' do
+      profile = create(:profile)
+      image_blob = FactoryBotHelpers.dummy_image('over_10_mb.jpg')
+      profile.header_image.attach(io: StringIO.new(image_blob),
+                                  filename: SecureRandom.uuid,
+                                  content_type: 'image/webp')
+      expect(profile.errors.full_messages).to include('Header image 画像は10MB以下のファイルを選択してください')
+    end
+
+    it 'header_imageの拡張子がwebpで登録できるか' do
+      profile = create(:profile)
+      image_blob = FactoryBotHelpers.dummy_image('dummy.png', 'webp')
+      profile.header_image.attach(io: StringIO.new(image_blob),
+                                  filename: SecureRandom.uuid,
+                                  content_type: 'image/webp')
+      expect(profile.errors.full_messages).to be_empty
+    end
+
+    it 'header_imageの拡張子がwebp以外で登録できないか' do
+      profile = create(:profile)
+      image_blob = FactoryBotHelpers.dummy_image('dummy.png', 'jpg')
+      profile.header_image.attach(io: StringIO.new(image_blob),
+                                  filename: SecureRandom.uuid,
+                                  content_type: 'image/jpg')
+      expect(profile.errors.full_messages).to include('Header image 画像形式はwebpのみ対応しています')
+    end
+  end
 end
