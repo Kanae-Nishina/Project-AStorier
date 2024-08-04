b3a68a971bc01dbe87c5a9731578093650dfdf02
topi0247 <karubona.ra247@gmail.com>
2024-08-04 15:23:12 +0900

    fix:illust_attachmentの修正

--- back/spec/factories/illust_attachments.rb
+++ back/spec/factories/illust_attachments.rb
@@ -2,11 +2,13 @@ FactoryBot.define do
   factory :illust_attachment do
     association :illust
     sequence(:position) { |n| n }
-    after(:create) do |illust_attachment|
-      image_blob = FactoryBotHelpers.dummy_image('dummy.png')
-      illust_attachment.image.attach(io: StringIO.new(image_blob),
-                                    filename: SecureRandom.uuid,
-                                    content_type: 'image/webp')
+    trait(:with_image) do
+      after(:build) do |illust_attachment|
+        image_blob = FactoryBotHelpers.dummy_image('dummy.png')
+        illust_attachment.image.attach(io: StringIO.new(image_blob),
+                                      filename: SecureRandom.uuid,
+                                      content_type: 'image/webp')
+      end
     end
   end
 end
